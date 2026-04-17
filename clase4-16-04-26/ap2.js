682b3e51cc0f220b138130968ac923292fd7256c
neribalbuena <nerybalbuena293@gmail.com>
2026-04-16 21:38:04 -0300

    Create ap2.js

---
+++ clase4-16-04-26/ap2.js
@@ -0,0 +1,9 @@
+export const fetchData = async () => {
+    try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        return await res.json();
+    } catch (e) {
+        console.log("Error:", e);
+    }
+};
+
